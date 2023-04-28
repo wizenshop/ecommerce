11c31906a2144b64e760707357ec41bafffdb31b
wizenshop <130877337+wizenshop@users.noreply.github.com>
2023-04-27 21:47:19 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# ecommerce
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-1uunxe)
---
+++ components/BottomNavigation/index.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import Link from "next/link";
+
+import * as Style from "./styles";
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+import { styled, keyframes, css } from "../../../styles";
+
+import { BiHomeAlt, BiUser } from "react-icons/bi";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+
+
+const activeItemStyles = {
+  color: "$colorActiveMenuIcon",
+};
+
+export function BottomNavigation() {
+ 
+
+  return (
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+        <MainNav items={siteConfig.mainNav} />
+        <div className="flex flex-1 items-center justify-end space-x-4">
+          <nav className="flex items-center space-x-1">
+            <Link
+              href={siteConfig.links.github}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div
+                className={buttonVariants({
+                  size: "sm",
+                  variant: "ghost",
+                })}
+              >
+                <Icons.gitHub className="h-5 w-5" />
+                <span className="sr-only">GitHub</span>
+              </div>
+            </Link>
+            <Link
+              href={siteConfig.links.twitter}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div
+                className={buttonVariants({
+                  size: "sm",
+                  variant: "ghost",
+                })}
+              >
+                <Icons.twitter className="h-5 w-5 fill-current" />
+                <span className="sr-only">Twitter</span>
+              </div>
+            </Link>
+            <ThemeToggle />
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
