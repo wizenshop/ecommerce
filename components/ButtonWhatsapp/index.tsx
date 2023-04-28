ec1d86171f55afe24f3da5ecf16824d44c86e12b
wizenshop <130877337+wizenshop@users.noreply.github.com>
2023-04-28 09:13:40 -0300

    2 files modified

--- components/ButtonWhatsapp/index.tsx
+++ components/ButtonWhatsapp/index.tsx
@@ -6,10 +6,14 @@ import { FloatingWhatsApp } from "react-floating-whatsapp";
 
 export function ButtonWhatsapp() {
   return (
-    <Suspense fallback={<p>.</p>}>
+    <Suspense fallback={<p>carregarnfo</p>}>
       <FloatingWhatsApp
         phoneNumber="85987520858"
         accountName="Online"
+        buttonStyle={{bottom: "5rem"}}
+        style={{height: "3rem",width:"3rem",position:"relative"}}
+        chatMessage="Óla"
+        placeholder="Digite uma mensagem.."
         allowEsc
         allowClickAway
         notification
