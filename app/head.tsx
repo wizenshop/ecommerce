a6e6fa631f4e9304b68ba395c03d8005c128cc31
wizenshop <130877337+wizenshop@users.noreply.github.com>
2023-04-28 23:07:21 -0300

    new

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+
+export default function Head() {
+  return (
+    <>
+      <meta http-equiv="X-UA-Compatible" content="IE=7" />
+      <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+    </>
+  );
+}
